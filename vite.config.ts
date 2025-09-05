a9320f3c2ca50b87450a21a58530d40149171d0a
Sebastian Gilian Alayo <sebastiangilian7@gmail.com>
2025-09-05 13:36:29 -0500

    chore(config): configuracion de los alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import path from "path";
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
       tailwindcss(),
   ],
+    resolve: {
+        alias: {
+            "@app": path.resolve(__dirname, "./src/app"),
+            "assets": path.resolve(__dirname, "./src/assets"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@config": path.resolve(__dirname, "./src/config"),
+            "@features": path.resolve(__dirname, "./src/features"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@lib": path.resolve(__dirname, "./src/lib"),
+            "@playground": path.resolve(__dirname, "./src/playground"),
+            "@testing": path.resolve(__dirname, "./src/testing"),
+            "@types/": path.resolve(__dirname, "./src/types"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+        }
+    }
 })
