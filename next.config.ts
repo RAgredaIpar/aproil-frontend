62158cc09d9bfe21d17958384311736a374a149d
Sebastian Gilian Alayo <sebastiangilian7@gmail.com>
2025-10-23 01:34:42 -0500

    Revert "feat(config): permitir carga de archivos desde supabase"
    This reverts commit e6c006b4ce031f27aa7bc3a56831b4f72d58ebd0.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            new URL('https://ffwgnnnkziftrmagpkka.supabase.co/**'),
-        ],
-    },
-};
+const nextConfig: NextConfig = {};
+
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
