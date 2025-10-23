ee686a16d7485caa81796f5e2399cc86d3babc03
Sebastian Gilian Alayo <sebastiangilian7@gmail.com>
2025-10-23 01:39:50 -0500

    fix(config): permitir carga de archivos desde supabase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        remotePatterns: [
+            new URL('https://ffwgnnnkziftrmagpkka.supabase.co/**'),
+        ],
+    },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
